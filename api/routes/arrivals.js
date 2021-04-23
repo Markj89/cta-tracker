3db7a51812e49b2b51103adff6a2a8c8db86e63f
Marcus Jackson <marcusjackson@Marcuss-MacBook-Pro.local>
2021-04-22 21:17:39 -0500

    Connected Backend /Arrivals to Front-End #29

--- api/routes/arrivals.js
+++ api/routes/arrivals.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const request = require("request");
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     const stationColor = (object, value) => {
         return Object.keys(object).find(key => object[key] === value && key !== 'ada');
     };
 
     var options = {
-        method: 'POST',
+        method: 'GET',
         url: process.env.ARRIVALS, // API Endpoint
         qs: {
             key: process.env.CTA_KEY, // API KEY
-            mapid: station.map_id,
-            rt: stationColor(station, true),
+            mapid: req.body.map_id,
+            rt: stationColor(req.body, true),
             outputType: 'json'
         },
         headers: {
@@ -30,6 +31,8 @@ router.get('/', (req, res) => {
         if (error) throw new Error(error);
         res.send(body);
     });
+
 });
+
 
 module.exports = router;
