6eb89f381ce306f558a8090d88810019e01c3b46
Markj89 <3435026+Markj89@users.noreply.github.com>
2024-07-28 18:52:08 -0500

    #70 Update styling for StationModal Card Component

--- src/components/Modal/StationModal.tsx
+++ src/components/Modal/StationModal.tsx
@@ -50,7 +50,7 @@ export default function StationModal({ station, position, style, isOpen, stops}:
                 { data !== null && loading === false ? (
                     <div className="space-y-4 station mb-1">
                         {data?.map((item) => (
-                            <ul className="w-72 list-inside rounded flex align-middle arrival-list-item px-3.5" style={{ backgroundColor: `${station?.color}`}}>
+                            <ul className="w-72 list-inside station-list-item flex align-middle arrival-list-item px-3.5" style={{ backgroundColor: `${station?.color}`}}>
                                 <li className="text-left flex-none w-24 text-white text-base">{item?.stpDe?.replace('Service toward ', '')}</li>
                                 <ul className="flex-auto w-32 arrival-list-item-times">
                                     <li className="text-right arrival-time text-white my-0 text-sm">{formatArrivalTime(item?.arrT).toString() === '0' ? 'Due' : `${formatArrivalTime(item?.arrT).toString()} mins`}</li>
